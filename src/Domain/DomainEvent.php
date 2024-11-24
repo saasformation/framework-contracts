a07bc2da45734c9b54398c99366a69b6588c85d6
SaaSFormation <info@saasformation.com>
2024-11-24 11:29:25 +0000

    feat: add read-model-related features

--- src/Domain/DomainEvent.php
+++ src/Domain/DomainEvent.php
@@ -7,13 +7,18 @@ use SaaSFormation\Framework\Contracts\UI\HTTP\ArrayableInterface;
 
 readonly abstract class DomainEvent implements ArrayableInterface
 {
-    public function __construct(private ?IdInterface $id = null)
+    public function __construct(private IdInterface $aggregateId, private ?IdInterface $id = null)
     {
     }
 
     public function id(): ?IdInterface
     {
         return $this->id;
+    }
+
+    public function aggregateId(): IdInterface
+    {
+        return $this->aggregateId;
     }
 
     public static abstract function code(): string;
