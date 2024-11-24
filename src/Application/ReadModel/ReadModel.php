082894ec686d6d984ebace96d9e56996d94815c0
SaaSFormation <info@saasformation.com>
2024-11-24 11:50:19 +0000

    feat: add read-model-related features

--- src/Application/ReadModel/ReadModel.php
+++ src/Application/ReadModel/ReadModel.php
@@ -2,8 +2,24 @@
 
 namespace SaaSFormation\Framework\Contracts\Application\ReadModel;
 
+use SaaSFormation\Framework\Contracts\Common\Identity\IdInterface;
+
 abstract class ReadModel
 {
+    public function __construct(private ?IdInterface $id = null)
+    {
+    }
+
+    public function id(): ?IdInterface
+    {
+        return $this->id;
+    }
+
+    public function setId(IdInterface $id): void
+    {
+        $this->id = $id;
+    }
+
     /**
      * @return array<mixed>
      */
