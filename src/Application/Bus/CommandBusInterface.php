0da44ac2bc445f29f6f3533d261b5411a56dd85f
SaaSFormation <info@saasformation.com>
2024-11-27 17:15:55 +0000

    feat: add behat extension

--- src/Application/Bus/CommandBusInterface.php
+++ src/Application/Bus/CommandBusInterface.php
@@ -2,8 +2,6 @@
 
 namespace SaaSFormation\Framework\Contracts\Application\Bus;
 
-use SaaSFormation\Framework\Contracts\Domain\DomainEventStream;
-
 interface CommandBusInterface
 {
     public function handle(CommandInterface $command): void;
