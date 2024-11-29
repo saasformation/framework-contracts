759256aa2f216a7b800c79ebd6208d1bb4b5b49e
SaaSFormation <info@saasformation.com>
2024-11-29 07:16:16 +0000

    fix: move processRequest and processError to APIServerInterface which where it should have belong from the beginning. Also, move Kernel outside API and now is fully compatible with a cli kernel.

--- src/Infrastructure/API/APIServerInterface.php
+++ src/Infrastructure/API/APIServerInterface.php
@@ -2,7 +2,14 @@
 
 namespace SaaSFormation\Framework\Contracts\Infrastructure\API;
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 interface APIServerInterface
 {
     public function start(): void;
+
+    public function processRequest(ServerRequestInterface $request): ResponseInterface;
+
+    public function processError(ServerRequestInterface $request, \Throwable $exception): ResponseInterface;
 }
--- src/Infrastructure/KernelInterface.php
+++ src/Infrastructure/KernelInterface.php
@@ -15,8 +15,4 @@ interface KernelInterface
     public function envVarsManager(): EnvVarsManagerInterface;
 
     public function container(): ContainerInterface;
-
-    public function processRequest(ServerRequestInterface $request): ResponseInterface;
-
-    public function processError(ServerRequestInterface $request, Throwable $exception): ResponseInterface;
 }
